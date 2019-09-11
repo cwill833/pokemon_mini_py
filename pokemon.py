f2ac57917fb2042e1463b8649158114ba5e683e7
Chris Williams <cwill833@gmail.com>
2019-09-10 23:35:49 -0500

    add regain health, lose health, revive, die functions to class

--- pokemon.py
+++ pokemon.py
@@ -6,5 +6,28 @@ class Pokemon:
 		self.max_health = max_health
 		self.current_health = current_health
 		self.is_knocked_out = is_knocked_out
-
 	
+	def dead(self):
+		if self.current_health <= 0:
+			self.is_knocked_out = True
+	
+	def alive(self):
+		if self.current_health == 0:
+			self.is_knocked_out = False
+			print(f"{self.name} is revived!")
+
+	def lose_health(self, amount):
+		self.current_health -= amount
+		self.dead()
+		string = f"{self.name} took {amount} damage!" if self.current_health > 0 else f"{self.name} took {amount} damage and fainted!"
+		print(string) 
+		
+	def regain_health(self, amount):
+		self.alive()
+		self.current_health += amount
+		print(f"{self.name} has gained {amount} HP!")
+
+pikacu = Pokemon('Pikacu', 25, 'electric', 50, 50, False)
+
+pikacu.lose_health(50)
+pikacu.regain_health(50)
