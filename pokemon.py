2504802e00518e39de9b2d10cf47a394468009cb
Chris Williams <cwill833@gmail.com>
2019-09-10 23:16:48 -0500

    created pokemon class with init funciton

---
+++ pokemon.py
@@ -0,0 +1,10 @@
+class Pokemon:
+	def __init__(self, name, level, element, max_health, current_health, is_knocked_out):
+		self.name = name
+		self.level = level
+		self.element = element
+		self.max_health = max_health
+		self.current_health = current_health
+		self.is_knocked_out = is_knocked_out
+
+	
